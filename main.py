8e30382f2aca1f25aed6c0e48db798b805c6094f
Hiccup <garciacassiohectorguillermo7@gmail.com>
2025-07-18 18:03:52 -0400

    complete inital v0.0

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
-# import dash
-# from dash import dcc, html
-# import plotly.express as px
-# import requests
-# import pandas as pd
+import dash
+from dash import dcc, html
+import plotly.express as px
+import requests
+import pandas as pd
 
-# # Initialize Dash app
-# app = dash.Dash(__name__)
+# Initialize Dash app
+app = dash.Dash(__name__)
 
-# # Fetch data for a given country
-# def get_country_data(country):
-#     url = f'https://disease.sh/v3/covid-19/countries/{country}'
-#     response = requests.get(url)
-#     return response.json()
+# Fetch data for a given country
+def get_country_data(country):
+    url = f'https://disease.sh/v3/covid-19/countries/{country}'
+    response = requests.get(url)
+    return response.json()
 
-# # Layout for the dashboard
-# app.layout = html.Div([
-#     dcc.Dropdown(
-#         id='country-dropdown',
-#         options=[
-#             {'label': 'USA', 'value': 'USA'},
-#             {'label': 'India', 'value': 'India'},
-#             {'label': 'Brazil', 'value': 'Brazil'},
-#             {'label': 'Russia', 'value': 'Russia'}
-#         ],
-#         value='USA',
-#         style={'width': '50%'}
-#     ),
-#     dcc.Graph(id='covid-graph')
-# ])
+# Layout for the dashboard
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[
+            {'label': 'USA', 'value': 'USA'},
+            {'label': 'India', 'value': 'India'},
+            {'label': 'Brazil', 'value': 'Brazil'},
+            {'label': 'Russia', 'value': 'Russia'}
+        ],
+        value='USA',
+        style={'width': '50%'}
+    ),
+    dcc.Graph(id='covid-graph')
+])
 
-# # Callback to update the graph based on the selected country
-# @app.callback(
-#     dash.dependencies.Output('covid-graph', 'figure'),
-#     [dash.dependencies.Input('country-dropdown', 'value')]
-# )
-# def update_graph(selected_country):
-#     data = get_country_data(selected_country)
-#     country_data = {
-#         'Category': ['Cases', 'Deaths', 'Recovered'],
-#         'Count': [data['cases'], data['deaths'], data['recovered']]
-#     }
-#     df_plot = pd.DataFrame(country_data)
+# Callback to update the graph based on the selected country
+@app.callback(
+    dash.dependencies.Output('covid-graph', 'figure'),
+    [dash.dependencies.Input('country-dropdown', 'value')]
+)
+def update_graph(selected_country):
+    data = get_country_data(selected_country)
+    country_data = {
+        'Category': ['Cases', 'Deaths', 'Recovered'],
+        'Count': [data['cases'], data['deaths'], data['recovered']]
+    }
+    df_plot = pd.DataFrame(country_data)
     
-#     # Plotly bar chart
-#     fig = px.bar(df_plot, x='Category', y='Count', title=f"COVID-19 Overview in {selected_country}")
-#     return fig
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port = 8051)
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
+    # Plotly bar chart
+    fig = px.bar(df_plot, x='Category', y='Count', title=f"COVID-19 Overview in {selected_country}")
+    return fig
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, port = 8051)
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return "Hello, World!"
+
+# if __name__ == '__main__':
+#     app.run(debug=False)
